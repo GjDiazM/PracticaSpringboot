ed2802473ce30c12af8e0ee1326f544853a9eeba
Gilberto Diaz <gjdiaz.2020@gmail.com>
2024-02-02 03:04:59 -0500

    Implemento el desarrollo de la tabla en HTML

--- Practica/src/main/java/com/Practica/Practica/controllers/UsuarioController.java
+++ Practica/src/main/java/com/Practica/Practica/controllers/UsuarioController.java
@@ -6,21 +6,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "usuario/{id}")
-    public Usuario GetUsuario(@PathVariable long id){
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> GetUsuario(){
+
+        List<Usuario>usuarios = new ArrayList<>();
+
+
         Usuario User1= new Usuario();
-        User1.setId(id);
+        User1.setId(230);
         User1.setNombre("Gilberto");
         User1.setApellido("Diaz");
         User1.setCiudad("Soledad");
         User1.setEmail("Gjdiaz@gmail.com");
         User1.setTelefono("3012312");
-        return User1;
+
+        Usuario User2= new Usuario();
+        User2.setId(550);
+        User2.setNombre("Godoy");
+        User2.setApellido("marquezz");
+        User2.setCiudad("Sol");
+        User2.setEmail("Gjdiazada@gmail.com");
+        User2.setTelefono("30112312");
+
+
+        usuarios.add(User1);
+        usuarios.add(User2);
+        return usuarios;
 
     }
     @RequestMapping(value = "usuario2")
--- Practica/src/main/resources/static/js/demo/datatables-demo.js
+++ Practica/src/main/resources/static/js/demo/datatables-demo.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 async function cargarusuarios(){
 
 
-  const rawResponse = await fetch('usuario/2015', {
+  const rawResponse = await fetch('usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -17,6 +17,18 @@ async function cargarusuarios(){
   const content = await rawResponse.json();
 
   console.log(content);
+  let listadohtml='';
+  for (let users of content){
+  let usersHTML='<tr><td>'+users.id+'</td><td>'+users.nombre+'  '+users.apellido+'</td><td>'+users.telefono+'</td><td>'+users.email+'</td><td>'+users.ciudad+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i> </a></td></tr>'
+
+    listadohtml +=usersHTML;
+  }
+
+
+
+
+  document.querySelector('#usuarios tbody').outerHTML= listadohtml;
+
 
 
 
